fd7b3acd9352c87dae759740e8482c76c11776d9
ydmins <dr.ydmins@gmail.com>
2023-09-11 15:42:38 +0900

    [ADD] PostsApiController, PostsApiControllerTest, PostsSaveRequestDto, PostsService
    JPA 활용한 데이터 저장 연습 및 테스트 코드

---
+++ src/main/java/com/ydmins/JPAbase2/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.ydmins.JPAbase2.service;
+
+import com.ydmins.JPAbase2.domain.posts.PostsRepository;
+import com.ydmins.JPAbase2.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
