d0f8d586d1bded43b86b9f9c937a3652e57eb6c4
ydmins <dr.ydmins@gmail.com>
2023-09-11 12:01:01 +0900

    [ADD] Posts, PostsRepository, PostsRepositoryTest
    SpringJPA 사용
    - @Entity를 이용해 테이블 설정
    - Repository 인터페이스를 이용해 Entity 클래스가 DB에 접근할 수 있도록 설정
    - Repository 테스트 작성

---
+++ src/test/java/com/ydmins/JPAbase2/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.ydmins.JPAbase2.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void content_save_read(){
+        // given
+        String title = "test";
+        String content = "test content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("JIN")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
