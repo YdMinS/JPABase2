7cae2836209d1090d3ed1e70a77d909eea630668
ydmins <dr.ydmins@gmail.com>
2023-09-11 11:02:14 +0900

    first commit

---
+++ src/main/java/com/ydmins/JPAbase2/JpAbase2Application.java
@@ -0,0 +1,13 @@
+package com.ydmins.JPAbase2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAbase2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAbase2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ydmins/JPAbase2/JpAbase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydmins.JPAbase2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAbase2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
